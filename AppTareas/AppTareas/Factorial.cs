39efd5e7375cb4ae9b6c4b797dbb561c9a80586e
jofremaza <jofremaza37@gmail.com>
2019-11-13 19:42:35 -0500

    Factorial Final

---
+++ AppTareas/AppTareas/Factorial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppTareas
+{
+    public partial class Factorial : Form
+    {
+        public Factorial()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            this.textBox2.Text = "";
+            this.textBox1.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            if (this.textBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Ingrse algún valor");
+                this.textBox1.Focus();
+                return;
+            }
+            num = int.Parse(this.textBox1.Text);
+            long fact = facnum(num);
+            this.textBox2.Text = fact.ToString();
+        }
+
+        private long facnum(int num )
+        {
+            int cont = 1;
+            for (int i = 1; i <= num; i++)
+                
+            {
+                cont = cont * i;
+            }
+            return cont;
+
+        }
+    }
+}
